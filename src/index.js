aeec4beb36cc107767e895c47d52ffe55cf29790
Ivan Starčević <istarcevic16@gmail.com>
2022-06-30 17:44:04 +0200

    no image fix

--- src/index.js
+++ src/index.js
@@ -18,40 +18,37 @@ function pozoviServer(e) {
 }
 
 function ubaciRezultateUDom(data, element) {
-  let rating = data[0].show.rating.average;
-  if (rating == null) {
-    rating = "Ovaj film nije još ocijenjen";
-  }
-
   let rezultat = `<div class="container">`;
   data.forEach((film) => {
-    if (film.show.image == null) {
-      console.log(
-        "ne znam zasto moram imati ovaj if da ovo radi s filmom 'tamara' npr"
-      );
-    } else {
-      let image = film.show.image.medium;
-      rezultat += ` 
+    // ako nema ratinga, ispisujemo ovaj tekst
+    let rating = film.show.rating.average;
+    if (rating == null) {
+      rating = "Ovaj film nije još ocijenjen";
+    }
+    // ako nema slike
+    let image = film.show.image
+      ? film.show.image.medium
+      : "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
+    rezultat += ` 
 
 <div class="card">
-       <div class="card__image">
-         <img
+      <div class="card__image">
+        <img
               src="${image}"
               class="img"
               alt="..."
             />
-     </div>
-     <div class="card__content">
+    </div>
+    <div class="card__content">
       <h2>${film.show.name}</h2>
       <div>
       <p class="summary"><span class="about">About</span>: ${film.show.summary}</p>
       </div>
       <p><span class="rating">Rating</span>: ${rating}<br>
       <span class="rating">Language</span>: ${film.show.language}</p>
-     </div>
-     </div>
+    </div>
+    </div>
       `;
-    }
   });
   rezultat += "</div>";
   element.innerHTML = rezultat;
